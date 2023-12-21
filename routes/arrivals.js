42954440a371f8def5b573992461eb31ed271dbc
csposito1220 <csposito1220@gmail.com>
2023-12-21 09:29:05 -0500

    commit

---
+++ routes/arrivals.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var router = express.Router();
+
+var arrivalsCtrl = require("../controllers/arrivals");
+
+router.post("/flights/:id/arrivals", arrivalsCtrl.create);
+
+module.exports = router;
